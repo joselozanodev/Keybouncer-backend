bd60b7be77534f46eb7b76483745b9b72d811043
Jose Lozano <joselozano.dev@gmail.com>
2024-06-11 16:38:26 -0400

    chore: Add auth, password, site, and user modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { PasswordModule } from './password/password.module';
+import { SiteModule } from './site/site.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, PasswordModule, SiteModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
